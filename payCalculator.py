fccfcaf6cbe0d49d743afcbe0438ec4780f846de
Brett McElvain <brett@Bretts-MacBook-Pro.local>
2022-02-20 12:11:19 -0600

    Created calculatePay().

--- payCalculator.py
+++ payCalculator.py
@@ -1,8 +1,13 @@
 def calculatePay():
     
     # This first line is provided for you
-    hrs = input("Enter Hours:")
-    
+    hrs = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))
+    overtime_hours = 0
+    if hrs > 40:
+        overtime_hours = hrs - 40
+    pay = (hrs * rate) + (overtime_hours * (rate * 1.5))
+    print("Pay: {}".format(pay))
     # end assignment
 
 ## if you want to test locally before you try to sync
